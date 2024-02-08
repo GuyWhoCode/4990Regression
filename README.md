35381bd93ff1edac2ee0c118d765e616b97109ec
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-02-07 22:30:45 -0800

    feat: clean data

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # 4990Regression
+
 Instructions to run file:
+
 1. `pipenv install`
 2. `pipenv shell`
-3. `pipenv run regression`
+3. `pipenv run regression` or `py regression.py`
+
+# 1. Problem Identification
+
+Problem: Predicting attendance at NFL games <br>
+Target Variable: Attendance <br>
+Features / Independent Variables: Weather, Current Win Rate, Strength of Opponent <br>
+
+# 2. Data Collection
+
+Data sets taken from Kaggle: 
+- [NFL Stadium Attendance](https://www.kaggle.com/datasets/sujaykapadnis/nfl-stadium-attendance-dataset) 
+- [Weather data for NFL games](https://www.kaggle.com/datasets/tombliss/weather-data)
+
+# 3. Model Development
+Used a linear regression and polynomial regression model to predict attendance at NFL games.
+
+
+# 4. Model Evaluation
--- regression.py
+++ regression.py
@@ -1,10 +1,47 @@
-import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-# y = 1 * x_0 + 2 * x_1 + 3
-y = np.dot(X, np.array([1, 2])) + 3
-reg = LinearRegression().fit(X, y)
-reg.score(X, y)
-reg.coef_
-reg.intercept_
-reg.predict(np.array([[3, 5]]))
+import json
+
+# Load the csv data using only the game_id, Temperature, and EstimatedCondition columns
+GAME_WEATHER = pd.read_csv(
+    'kaggleData/games_weather.csv', header=0,
+    usecols=["game_id", "Temperature", "EstimatedCondition"])
+
+# CSV data with only the game_id, Season, and StadiumName columns
+GAMES_DATA = pd.read_csv(
+    'kaggleData/games.csv', header=0,
+    usecols=["game_id", "Season", "StadiumName"])
+
+# CSV data with only the team, year, week, and weekly_attendance columns
+ATTENDANCE = pd.read_csv(
+    'kaggleData/attendance.csv',
+    header=0, usecols=["team", "year", "week", "weekly_attendance"])
+
+# CSV data with only the team, year, and simple_rating columns
+STANDINGS = pd.read_csv(
+    'kaggleData/standings.csv', header=0,
+    usecols=["team", "year", "simple_rating"])
+
+# Merge game weather and data together based on game_id
+COMBINED_GAME_WEATHER_DATA = pd.merge(GAME_WEATHER, GAMES_DATA, on='game_id')
+
+# Delete the 'game_id' column
+COMBINED_GAME_WEATHER_DATA = COMBINED_GAME_WEATHER_DATA.drop('game_id', axis=1)
+
+# Merge attendance and standings data together based on team and year
+COMBINED_ATTENDANCE_DATA = pd.merge(ATTENDANCE, STANDINGS, on=['team', 'year'])
+
+with open('stadium.json') as f:
+    stadium_data = json.load(f)
+    COMBINED_GAME_WEATHER_DATA['StadiumName'] = COMBINED_GAME_WEATHER_DATA['StadiumName'].replace(
+        stadium_data)
+    COMBINED_GAME_WEATHER_DATA.rename(
+        columns={'StadiumName': 'team'}, inplace=True)
+    # Normalizes the data by replacing the stadium names with the team names using a dictionary
+
+    COMBINED_GAME_WEATHER_DATA.rename(columns={'Season': 'year'}, inplace=True)
+    # Further normalize data by replacing the 'Season' column with 'year'
+
+    TRAINING_DATA = pd.merge(COMBINED_GAME_WEATHER_DATA,
+                             COMBINED_ATTENDANCE_DATA, on=['team', 'year'])
+    print(TRAINING_DATA.head())
